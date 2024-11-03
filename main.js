fafac665b3005fa0022eb390446444ff9dd7a5e6
Roan Ali <alir13@usf.edu>
2024-11-02 21:03:28 -0400

    Implement async/await to fetch ticket data and handle errors.

---
+++ main.js
@@ -0,0 +1,29 @@
+//Task 2:Fetch Tickets Using Async/Await and Handle Errors
+
+async function fetchTickets() {
+    const api = "https://jsonplaceholder.typicode.com/posts";
+    const errorMessage = document.getElementById("error-message");
+
+    try {
+        const response = await fetch(api);
+        
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+
+        const tickets = await response.json();
+
+        if (tickets.length === 0) {
+            throw new Error("Tickets could not be found.");
+        }
+
+        displayTickets(tickets);
+
+    } catch (error) {
+        errorMessage.style.display = "Error fetching data";
+        errorMessage.textContent = error.message;
+    }
+}
+
+fetchTickets();
+
